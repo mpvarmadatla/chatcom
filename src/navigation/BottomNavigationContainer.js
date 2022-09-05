ac0b15ce83b992488ea5df0fed36b69082a49ed8
manoj <mpvd2049@gmail.com>
2022-09-05 14:43:00 +0530

    add icons to bottomsheet

--- src/navigation/BottomNavigationContainer.js
+++ src/navigation/BottomNavigationContainer.js
@@ -5,19 +5,40 @@ import CallBottomScreen from '../screens/BottomScreens/CallBottomScreen';
 import CameraBottomScreen from '../screens/BottomScreens/CameraBottomScreen';
 import ChatBottomScreen from '../screens/BottomScreens/ChatBottomScreen';
 import AccountBottomScreen from '../screens/BottomScreens/AccontBottomScreen';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 const BottomNavigationContainer = () =>{
     const Tab = createBottomTabNavigator()
     const options = {
-        headerShown:false
+        headerShown:false,
     }
     return (
         <Tab.Navigator>
-            <Tab.Screen name='Groups' component={GroupsBottomScreen} options={options} />
-            <Tab.Screen name='Call'  component={CallBottomScreen} options={options}/>
-            <Tab.Screen name='Camera' component={CameraBottomScreen}  options={options}/>
-            <Tab.Screen name ='Chats'  component={ChatBottomScreen} options={options}/>
-            <Tab.Screen name='Account' component={AccountBottomScreen} options={options}/>
+            <Tab.Screen name='Groups'
+             component={GroupsBottomScreen} 
+             options={{...options,tabBarIcon: ({color,size}) =>(
+                <MaterialIcon name="group" color={color} size={size}/>
+            )}} />
+            <Tab.Screen name='Call' 
+             component={CallBottomScreen} 
+             options={{...options,tabBarIcon: ({color,size}) =>(
+                <MaterialIcon name="call" color={color} size={size}/>
+            )}} />
+            <Tab.Screen name='Camera'
+             component={CameraBottomScreen}
+             options={{...options,tabBarIcon: ({color,size}) =>(
+                <MaterialIcon name="camera-alt" color={color} size={size}/>
+            )}}/>
+            <Tab.Screen name ='Chats' 
+             component={ChatBottomScreen} 
+             options={{...options,tabBarIcon: ({color,size}) =>(
+                <MaterialIcon name="chat" color={color} size={size}/>
+            )}}/>
+            <Tab.Screen name='Account' 
+            component={AccountBottomScreen}
+            options={{...options,tabBarIcon: ({color,size}) =>(
+                <MaterialIcon name="account-box" color={color} size={size}/>
+            )}}/>
         </Tab.Navigator>
     )
 }
